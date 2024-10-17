67e4013939ab521766788967374ddf16420ed593
Daan Rosies <daan307rosies@gmail.com>
2024-10-17 18:16:21 +0200

    Added read() method to the Artifact class
    Added read method and attributes for the Artifact method to return.
    The read method returns the data in binary format. For now no
    other implementation has been added to the Artifact class.
    The artifact class does still need to be edited.
    
    Daan Rosies (S5631742)

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -1,5 +1,11 @@
+from typing import Any
 from pydantic import BaseModel, Field
 import base64
 
+
 class Artifact(BaseModel):
-    pass
+    asset_path: str
+    data: base64
+
+    def read(self) -> base64:
+        return self.data

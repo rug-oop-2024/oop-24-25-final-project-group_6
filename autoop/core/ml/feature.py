aa1223b13a37e32ee147ba7f04f52411098458cb
Daan Rosies <daan307rosies@gmail.com>
2024-10-17 18:19:20 +0200

    Implemented the feature class
    Implemented the feature class. Which has two properties for now
    the type property which defines which type of feature column it is
    numerical or catigorical. The name property which is the name
    of the feature column. Furthermore, type can only take two values
    with are numerical or categorical any other value will raise a valueError.
    
    The class still needs to be edited for style conventions, docstrings
    and other implementations. As well as, best case OOP practises.
    
    Daan Rosies (S5631742)

--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -1,12 +1,31 @@
 
 from pydantic import BaseModel, Field
+from pydantic.fields import PrivateAttr
 from typing import Literal
 import numpy as np
 
 from autoop.core.ml.dataset import Dataset
 
-class Feature(BaseModel):
-    # attributes here
+
+class Feature():
+    _type: str
+    name: str
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @type.setter
+    def type(self, value: str):
+        if value not in ["numerical", "categorical"]:
+            raise ValueError("Type can only be either numerical or " +
+                             "categorical")
+        else:
+            self._type = value
+
+    def __init__(self, type: str, name: str):
+        self._type = type
+        self.name = name
 
     def __str__(self):
         raise NotImplementedError("To be implemented.")

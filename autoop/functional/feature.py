adc663c0211ed87d7abc268109e0641451fab2aa
Daan Rosies <daan307rosies@gmail.com>
2024-10-17 15:52:24 +0200

    Implement detect_feature_types function
    detect_feature_types detects the features for each feature column.
    The feature types it detects is numerical and categorical. Then stores
    it in a list of data type Feature, Feature class is an wrapper that
    stores the name of the data column and the feature type of the data
    column. Then returns this list of Feature objects.
    
    Daan Rosies (S5631742)
    15:55 | 10/17/2024

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -2,6 +2,8 @@
 from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
+import pandas as pd
+
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
@@ -10,4 +12,19 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     Returns:
         List[Feature]: List of features with their types.
     """
-    raise NotImplementedError("This should be implemented by you.")
+    data_frame: pd.DataFrame = dataset.read()
+    feature_types_list: List[Feature] = []
+
+    for col in data_frame.columns:
+        if data_frame[col].dtype in ['int64', 'float64']:
+            feature_types_list.append(Feature("numerical",
+                                              data_frame[col].name))
+        else:
+            feature_types_list.append(Feature("categorical",
+                                              data_frame[col].name))
+
+    return feature_types_list
+
+
+if __name__ == "__main__":
+    detect_feature_types(Dataset)

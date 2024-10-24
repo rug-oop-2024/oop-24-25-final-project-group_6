945f16a61f0ec6a3ecea325af79d3790a5b047cf
Daan Rosies <daan307rosies@gmail.com>
2024-10-24 12:22:09 +0200

    Style adjustments metric.py and pipeline.py
    Changed source code, to use flake8 style guidelines.
    
    Daan Rosies (S5631742)

--- autoop/core/ml/metric.py
+++ autoop/core/ml/metric.py
@@ -5,12 +5,14 @@ import numpy as np
 METRICS = [
     "mean_squared_error",
     "accuracy",
-] # add the names (in strings) of the metrics you implement
+]  # add the names (in strings) of the metrics you implement
+
 
 def get_metric(name: str):
     # Factory function to get a metric by name.
     # Return a metric instance given its str name.
     raise NotImplementedError("To be implemented.")
+
 
 class Metric(...):
     """Base class for all metrics.
@@ -22,4 +24,3 @@ class Metric(...):
         raise NotImplementedError("To be implemented.")
 
 # add here concrete implementations of the Metric class
-    

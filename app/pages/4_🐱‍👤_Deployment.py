32cd8e06124b5ae9a5bb9e1f6c0652d3bf0b4e29
DRS05 <79338608+DRS05@users.noreply.github.com>
2024-11-03 00:42:01 +0100

    Added deployment.py

---
+++ app/pages/4_🐱‍👤_Deployment.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle as pkl
+from typing import List
+
+from app.core.system import AutoMLSystem
+from app.deployment.load import select_pipeline
+from app.modelling.pipeline import display_pipeline_summary
+from autoop.core.ml.dataset import Dataset
+
+automl = AutoMLSystem.get_instance()
+
+available_pipelines: List[Dataset] = automl.registry.list(type="pipeline")
+
+
+def write_helper_text(text: str):
+    st.write(f"<p style=\"color: #888;\">{text}</p>", unsafe_allow_html=True)
+
+
+st.title("🛠 Pipeline manager")
+write_helper_text("In this section, you can design a machine learning " +
+                  "pipeline to train a model on a dataset.")
+
+pipeline_artifact = select_pipeline(available_pipelines)
+pipeline_data = pkl.loads(pipeline_artifact.data)
+display_pipeline_summary(
+    selected_dataset=pipeline_data["dataset"],
+    selected_feature=pipeline_data["feature_column"],
+    split_ratio=pipeline_data["split_ratio"],
+    selected_metrics=pipeline_data["selected_metrics"],
+    selected_model=pipeline_data["selected_model"]
+)

b43e6ee6bc6688f305a214392ac609aef8fc5cd9
DRS05 <79338608+DRS05@users.noreply.github.com>
2024-11-03 00:36:18 +0100

    Added select_pipeline
    Function for handling the selection of pipelines from a list of
    Artifacts of type pipeline. The pipeline that is selected is
    returned.
    
    Daan Rosies (S5631742)

---
+++ app/deployment/load.py
@@ -0,0 +1,23 @@
+from typing import List, Dict
+
+from autoop.core.ml.artifact import Artifact
+
+import streamlit as st
+
+
+def select_pipeline(pipelines: List[Artifact]) -> Artifact:
+    pipeline_contents: Dict = {}
+
+    for pipeline in pipelines:
+        pipeline_contents[pipeline.name] = pipeline
+
+    selected_pipeline_box = st.selectbox(
+        label="Select a pipeline:",
+        options=[pipeline + " (version: " +
+                 pipeline_contents[pipeline].version + ")"
+                 for pipeline in pipeline_contents.keys()]
+    )
+
+    selected_pipeline_name = selected_pipeline_box.split(" ")[0]
+
+    return pipeline_contents[selected_pipeline_name]

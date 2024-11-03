2e8c8c03c79da39532ad71d50ddeec18260c1784
Daan Rosies <daan307rosies@gmail.com>
2024-11-03 13:01:51 +0100

    Added a new test for metrics test_metric

--- autoop/tests/main.py
+++ autoop/tests/main.py
@@ -2,6 +2,7 @@ from autoop.tests.test_database import TestDatabase
 from autoop.tests.test_storage import TestStorage
 from autoop.tests.test_features import TestFeatures
 from autoop.tests.test_pipeline import TestPipeline
+from autoop.tests.test_metric import TestMetric
 
 import unittest
 

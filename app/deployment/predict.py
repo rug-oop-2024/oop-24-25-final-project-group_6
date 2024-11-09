9be57a85625250938c8a53733432faf03512ec70
Daan Rosies <daan307rosies@gmail.com>
2024-11-09 13:00:42 +0100

    Added docstrings
    The function still needs to be implemented properly.
    
    Daan Rosies (S5631742)

--- app/deployment/predict.py
+++ app/deployment/predict.py
@@ -2,15 +2,23 @@ from autoop.core.ml.model.model import Model
 from autoop.core.ml.dataset import Dataset
 from autoop.functional.preprocessing import preprocess_features
 from autoop.functional.feature import detect_feature_types
+
 import streamlit as st
-import pandas as pd
 
 
-def predict(model: Model, dataset: Dataset):
+def predict(model: Model, dataset: Dataset) -> None:
+    """
+    Function for predicting variables. Needs to be implemented.
+
+    Args:
+        model (Model): The model to predict from.
+        dataset (Dataset): The data to predict with.
+    """
     try:
         data = preprocess_features(detect_feature_types(dataset), dataset)
         for d in data:
-            if d[2]["type"] == "OneHot"
+            if d[2]["type"] == "OneHot":
+                return None
         st.write(data)
         model.predict(dataset.read())
     except Exception as e:

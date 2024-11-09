cd2b891042ff15bcd9a3dea25691e96e6c28ebab
Daan Rosies <daan307rosies@gmail.com>
2024-11-09 13:04:51 +0100

    Added docstrings

--- autoop/tests/test_metric.py
+++ autoop/tests/test_metric.py
@@ -1,6 +1,3 @@
-import unittest
-import numpy as np
-
 from autoop.core.ml.metric import (
     Metric,
     Accuracy,
@@ -11,14 +8,28 @@ from autoop.core.ml.metric import (
     RSquared
 )
 
+import numpy as np
+from typing import List
+import unittest
+
 
 class TestMetric(unittest.TestCase):
+    """
+    Class that unit tests metrics.
+    """
     def _test_metric(
             self,
             metric: Metric,
             task_type: str,
-            expected_results
+            expected_results: List[int]
             ) -> None:
+        """
+        Private method that handles the testing of metrics.
+        Args:
+            metric (Metric): The metric that is used
+            task_type (str): Task type of categorical or continuous
+            expected_results (List[int]): The results the metric expects.
+        """
         metric: Metric = metric()
         if task_type == "categorical":
             result_perfect = metric(np.array([1, 1, 1, 1, 1]),
@@ -44,24 +55,46 @@ class TestMetric(unittest.TestCase):
             result_failure = metric(np.array([0, 100, 200, 300, 400]),
                                     np.array([0, 0, 0, 0, 0]))
 
-            self.assertAlmostEqual(result_perfect, expected_results[0], delta=0.1)
-            self.assertAlmostEqual(result_ok, expected_results[1], delta=0.1)
-            self.assertAlmostEqual(result_failure, expected_results[2], delta=0.1)
+            self.assertAlmostEqual(result_perfect, expected_results[0],
+                                   delta=0.1)
+            self.assertAlmostEqual(result_ok, expected_results[1],
+                                   delta=0.1)
+            self.assertAlmostEqual(result_failure, expected_results[2],
+                                   delta=0.1)
 
     def test_accuracy_metric(self) -> None:
+        """
+        Tests the accuracy metric.
+        """
         self._test_metric(Accuracy, "categorical", [1, 0.6, 0])
 
     def test_precision_metric(self) -> None:
+        """
+        Tests the precision metric.
+        """
         self._test_metric(Precision, "categorical", [1, 0.66, 0])
 
     def test_recall_metric(self) -> None:
+        """
+        Tests the recall metric.
+        """
         self._test_metric(Recall, "categorical", [1, 0.66, 0])
 
     def test_mean_squared_error(self) -> None:
-        self._test_metric(MeanSquaredError, "continuous", [0.0, 425.2, 60000.0])
+        """
+        Tests the mean squared error metric.
+        """
+        self._test_metric(MeanSquaredError, "continuous",
+                          [0.0, 425.2, 60000.0])
 
     def test_mean_absolute_error(self) -> None:
+        """
+        Tests the mean absolute error metric.
+        """
         self._test_metric(MeanAbsoluteError, "continuous", [0.0, 15.2, 200.0])
 
     def test_r_squared_error(self) -> None:
+        """
+        Tests the r squared error metric.
+        """
         self._test_metric(RSquared, "continuous", [1.0, 0.97874, 0.0])
--- autoop/tests/test_storage.py
+++ autoop/tests/test_storage.py
@@ -1,35 +1,53 @@
-import unittest
-
 from autoop.core.storage import LocalStorage, NotFoundError
+
+import os
 import random
 import tempfile
+import unittest
+
 
 class TestStorage(unittest.TestCase):
+    """
+    Class that is used for testing the LocalStorage class.
+    """
+    def setUp(self) -> None:
+        """
+        Method that is ran before the tests begin.
 
-    def setUp(self):
+        A temporary dictonary and a storage is created for testing purposes.
+        """
         temp_dir = tempfile.mkdtemp()
         self.storage = LocalStorage(temp_dir)
 
-    def test_init(self):
+    def test_init(self) -> None:
+        """
+        Tests the initalizer method of the LocalStorage class
+        """
         self.assertIsInstance(self.storage, LocalStorage)
 
-    def test_store(self):
+    def test_store(self) -> None:
+        """
+        Tests the store method of the LocalStorage class.
+        """
         key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
-        key = "test/path"
+        key = f"test{os.sep}path"
         self.storage.save(test_bytes, key)
         self.assertEqual(self.storage.load(key), test_bytes)
-        otherkey = "test/otherpath"
+        otherkey = f"test{os.sep}otherpath"
         # should not be the same
         try:
             self.storage.load(otherkey)
         except Exception as e:
             self.assertIsInstance(e, NotFoundError)
 
-    def test_delete(self):
+    def test_delete(self) -> None:
+        """
+        Tests the delete method of the LocalStorage class.
+        """
         key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
-        key = "test/path"
+        key = f"test{os.sep}path"
         self.storage.save(test_bytes, key)
         self.storage.delete(key)
         try:
@@ -37,13 +55,16 @@ class TestStorage(unittest.TestCase):
         except Exception as e:
             self.assertIsInstance(e, NotFoundError)
 
-    def test_list(self):
+    def test_list(self) -> None:
+        """
+        Tests the list method of the LocalStorage class.
+        """
         key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
-        random_keys = [f"test/{random.randint(0, 100)}" for _ in range(10)]
+        random_keys = [f"test{os.sep}{random.randint(0, 100)}"
+                       for _ in range(10)]
         for key in random_keys:
             self.storage.save(test_bytes, key)
         keys = self.storage.list("test")
-        keys = ["/".join(key.split("/")[-2:]) for key in keys]
+        keys = [f"{os.sep}".join(key.split(f"{os.sep}")[-2:]) for key in keys]
         self.assertEqual(set(keys), set(random_keys))
-            

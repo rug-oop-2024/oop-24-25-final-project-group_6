3d45d8ae9d181977128e685b992ba68b5b958c52
Daan Rosies <daan307rosies@gmail.com>
2024-11-09 13:01:23 +0100

    Added docstrings

--- app/modelling/models.py
+++ app/modelling/models.py
@@ -1,15 +1,23 @@
-import streamlit as st
 from autoop.core.ml.feature import Feature
 from autoop.core.ml.model.model import Model
-
 from autoop.core.ml.model import (
     REGRESSION_MODELS,
     CLASSIFICATION_MODELS,
     get_model
 )
 
+import streamlit as st
+
 
 def select_model(feature: Feature) -> Model:
+    """
+    Function for selecting a model, depending on the type of feature.
+
+    Args:
+        feature (Feature): The type of feature either categorical or numerical.
+    Returns:
+        Model: The model that is selected from the streamlit selection box.
+    """
     if feature.type == "categorical":
         selected_model = st.selectbox("Choose a model:",
                                       options=CLASSIFICATION_MODELS)
